396374eb04eaf87df65227072664dcaeb3cb5ce3
Ivoine <ivoinestrachan242@gmail.com>
2024-04-12 16:42:31 -0700

    fixing footer

--- site/components/Footer.js
+++ site/components/Footer.js
@@ -1,9 +1,13 @@
+import Image from "next/image";
+import heart from "../public/heart.svg";
+
 const Footer = () => {
   return (
     <div>
-      built with ❤️ by real makers figuring out electronics
+      built with <Image src={heart} alt="heart" height={30} width={30} /> by real makers figuring out
+      electronics
     </div>
   );
-}
+};
 
 export default Footer;
